b16b4bb51fa250a5969bf669d692b348e96c7ecc
Aika Matyeva <matyevaa@gmail.com>
2020-03-19 17:32:49 -0700

    Test 2
    Homework/Test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,11 +1,21 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class MainClassTest extends MainClass{
+public class MainClassTest extends MainClass {
 
     @Test
-    public void testGetLocalNumber(){
-        assertEquals("Number should be equal to 14", 14, this.getLocalNumber());
+    public void testGetClassNumber() {
+        int expected = 45;
+
+        if(getClassNumber() < expected) {
+            Assert.fail("Number is not greater than " + expected);
+        }
+    }
+
+    @Test
+    public void testGetLocalNumber() {
+        assertEquals("Number should be equal to 14", 14, getLocalNumber());
     }
 }
